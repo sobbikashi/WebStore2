dd0689ea660e38a8d8aa2aa9a7d6dae76c9d98e4
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-09 13:06:58 +0300

    Заготовка контроллера

---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/employees")]
+public class EmployeesApicontroller: ControllerBase
+{
+    private readonly IEmployeesData _EmployeesData;
+    private readonly ILogger<EmployeesApicontroller> _Logger;
+
+    public EmployeesApicontroller(IEmployeesData EmployeesData, ILogger<EmployeesApicontroller> Logger)
+    {
+        _EmployeesData = EmployeesData;
+        _Logger = Logger;
+    }
+}
+
