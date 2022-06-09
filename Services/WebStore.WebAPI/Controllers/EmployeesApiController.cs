cda537786ca18a3b63df2ccdaf0dbeb48097adf3
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-09 13:14:25 +0300

    Заготовка контроллера WebAPI управления сотрудниками

---
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+public class EmployeesApiController : ControllerBase
+{
+    private readonly IEmployeesData _EmployeesData;
+    private readonly ILogger<EmployeesApiController> _Logger;
+
+    public EmployeesApiController(IEmployeesData EmployeesData, ILogger<EmployeesApiController> Logger)
+    {
+        _EmployeesData = EmployeesData;
+        _Logger = Logger;
+    }
+}
