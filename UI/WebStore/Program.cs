ee04cd1f1b4cbd5ed2e98b0f61072a8dfc028715
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-22 16:34:50 +0300

    Регистрация клиента webAPI сервиса заказов в основном приложении

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -13,6 +13,7 @@ using WebStore.Services.Services;
 using WebStore.Services.Services.InMemory;
 using WebStore.Services.Services.InSQL;
 using WebStore.WebAPI.Clients.Employees;
+using WebStore.WebAPI.Clients.Orders;
 using WebStore.WebAPI.Clients.Products;
 using WebStore.WebAPI.Clients.Values;
 
@@ -91,11 +92,13 @@ services.AddAuthorization(opt =>
 //services.AddScoped<IProductData, SqlProductData>();
 
 services.AddScoped<ICartService, InCookiesCartService>();
-services.AddScoped<IOrderService, SqlOrderService>();
+//services.AddScoped<IOrderService, SqlOrderService>();
 
 services.AddHttpClient<IValuesService, ValuesClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
 services.AddHttpClient<IEmployeesData, EmployeesClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
 services.AddHttpClient<IProductData, ProductsClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
+services.AddHttpClient<IOrderService, OrdersClient>(client => client.BaseAddress = new(configuration["WebAPI"]));
+
 
 //services.AddAutoMapper(Assembly.GetEntryAssembly());
 services.AddAutoMapper(typeof(Program));
