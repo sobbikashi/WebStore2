4d6a6812c4637bc5c241d7dac86bdac2f962f5b3
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-22 13:09:11 +0300

    DTO-модели данных для WebAPI каталога товаров

---
+++ Common/WebStore.Domain/DTO/ProductDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace WebStore.Domain.DTO
+{
+    public class ProductDTO
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Order { get; set; }
+                            
+        public SectionDTO Section { get; set; } = null!;
+                
+
+        public BrandDTO Brand { get; set; }
+
+        
+        public string ImageUrl { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+    public class SectionDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Order { get; set; }
+        public int? ParentId { get; set; }
+    }
+    public class BrandDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Order { get; set; }
+    }
+}
