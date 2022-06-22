9902fef5e2b1b9c652d07bdc912bfecbe7c9eb07
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-22 16:08:26 +0300

    Клиент для сервиса заказов

--- Services/WebStore.WebAPI.Clients/Orders/OrdersClient.cs
+++ Services/WebStore.WebAPI.Clients/Orders/OrdersClient.cs
@@ -1,4 +1,6 @@
-﻿using WebStore.Domain.Entities.Orders;
+﻿using System.Net.Http.Json;
+using WebStore.Domain.DTO;
+using WebStore.Domain.Entities.Orders;
 using WebStore.Interfaces.Services;
 using WebStore.ViewModels;
 using WebStore.WebAPI.Clients.Base;
@@ -12,22 +14,33 @@ public class OrdersClient : BaseClient, IOrderService
 
     }
 
-    public Task<IEnumerable<Order>> GetUserOrdersAsync(string UserName, CancellationToken Cancel = default)
+    public async Task<IEnumerable<Order>> GetUserOrdersAsync(string UserName, CancellationToken Cancel = default)
     {
-        throw new NotImplementedException();
+        var orders = await GetAsync<IEnumerable<OrderDTO>>($"{Address}/user/{UserName}").ConfigureAwait(false);
+        return orders!.FromDTO()!;
     }
 
-    public Task<Order?> GetOrderByIdAsync(int Id, CancellationToken Cancel = default)
+    public async Task<Order?> GetOrderByIdAsync(int Id, CancellationToken Cancel = default)
     {
-        throw new NotImplementedException();
+        var order = await GetAsync<OrderDTO>($"{Address}/{Id}");
+        return order.FromDTO();
     }
 
-    public Task<Order> CreateOrderAsync(string UserName, CartViewModel Cart, OrderViewModel OrderModel, CancellationToken Cancel = default)
+    public async Task<Order> CreateOrderAsync(string UserName, CartViewModel Cart, OrderViewModel OrderModel, CancellationToken Cancel = default)
     {
-        throw new NotImplementedException();
-    }
+        var model = new CreateOrderDTO
+        {
+            Items = Cart.ToDTO(),
+            Order = OrderModel,
+        };
+        var response = await PostAsync($"{Address}/{UserName}", model).ConfigureAwait(false);
+        var order = await response
+            .EnsureSuccessStatusCode()
+            .Content
+            .ReadFromJsonAsync<OrderDTO>(cancellationToken: Cancel)
+            .ConfigureAwait(false);
 
-    
-
-    
+        return order.FromDTO()!;
+    }  
+        
 }
