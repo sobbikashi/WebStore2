d673b07a7c2469f91f5c9ed3be9ff9c7cd273a4d
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-27 13:52:08 +0300

    Заготовки контроллеров Web API системы Identity

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route("api'roles")]
+public class RolesApiController : ControllerBase
+{
+    
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route("api/users")]
+public class UsersApiController : ControllerBase
+{
+
+}
+    
+
