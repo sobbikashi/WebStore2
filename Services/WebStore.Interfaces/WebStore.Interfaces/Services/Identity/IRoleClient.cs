e6f5435343a121d6614eae07627b4cd1a8e27f6c
Vincent Valentine <vanilmirth@inbox.ru>
2022-06-27 13:30:22 +0300

    Интерфейсы - фронт работ

---
+++ Services/WebStore.Interfaces/WebStore.Interfaces/Services/Identity/IRoleClient.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+internal interface IRoleClient : IRoleStore<Role> { }
+
---
+++ Services/WebStore.Interfaces/WebStore.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+public interface IUsersClient:
+    IUserRoleStore<User>, 
+    IUserPasswordStore<User>,
+    IUserEmailStore<User>,
+    IUserPhoneNumberStore<User>,
+    IUserTwoFactorStore<User>,
+    IUserLoginStore<User>,
+    IUserClaimStore<User>
+{
+
+}
+
+public interface IRolesCllient :
