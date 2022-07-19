a57195adf20781fb89652cbd5affb5e0fbdeb2de
Vincent Valentine <vanilmirth@inbox.ru>
2022-07-19 13:26:42 +0300

    DTO-модели для системы Identity, 56:52

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public class ClaimDTO : UserDTO
+{
+    public IEnumerable<ClaimDTO> Claims { get; set; } = null!;
+}
+
+public class ReplaceClaimDTO : UserDTO
+{
+    public Claim Claim { get; set; } = null!;
+    public Claim NewClaim { get; set; } = null!;
+}
+
