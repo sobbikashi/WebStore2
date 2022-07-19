7b9468324cdf9bcb4a07a5716d5b9c4ed9f3e262
Vincent Valentine <vanilmirth@inbox.ru>
2022-07-19 13:26:42 +0300

    DTO-модели для системы Identity

---
+++ Common/WebStore.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public abstract class UserDTO
+{
+    public User User { get; set; } = null!;
+}
+
+public class AddLoginDTO : UserDTO
+{
+    public UserLoginInfo UserLoginInfo { get; set; } = null!;
+}
+
+public class PasswordHashDTO : UserDTO
+{
+    public string Hash { get; set; } = null!;
+}
+
+public class SetLockoutDTO : UserDTO
+{
+    public DateTimeOffset? LockoutEnd { get; set; }
+}
