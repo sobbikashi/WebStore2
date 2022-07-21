b729657365d8c36f364cdd0835f229a8206d92ca
Vincent Valentine <vanilmirth@inbox.ru>
2022-07-21 14:13:10 +0300

    временный стоп

--- Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -23,6 +23,39 @@ public class UsersApiController : ControllerBase
 
     [HttpGet("all")]
     public async Task<IEnumerable<User>> GetAll() => await _UserStore.Users.ToArrayAsync();
+
+
+
+    #region Users
+
+    [HttpPost("UserID")]  //POST : api/v1/users/UserID
+    public async Task<string> GetUserIdAsync([FromBody] User user) => await _UserStore.GetUserIdAsync(user);
+
+
+    [HttpPost("UserName")]
+    public async Task<string> GetUserNameAsync([FromBody] User user) => await _UserStore.GetUserNameAsync(user);
+
+    [HttpPost("UserName/{name}")]
+    public async Task<string> SetUserNameAsync([FromBody] User user, string name)
+    {
+        await _UserStore.SetUserNameAsync(user, name);
+        await _UserStore.UpdateAsync(user);
+        return user.UserName;
+    }
+
+    [HttpPost("NormalUserName")]
+    public async Task<string> GetNormalizedUserNameAsync([FromBody] User user) => await _UserStore.GetNormalizedUserNameAsync(user);
+
+    [HttpPost("NormalUserName/{name}")]
+    public async Task<string> SetNormalizedUserNameAsync([FromBody] User user, string name)
+    {
+        await _UserStore.SetNormalizedUserNameAsync(user, name);
+        await _UserStore.UpdateAsync(user);
+        return user.NormalizedUserName;
+    }
+
+    #endregion
 }
-    
+
+
 
